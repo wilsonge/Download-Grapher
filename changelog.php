bbb942220f2cba8985e2cbbc902aedb971911350
wilsonge <admin@joomjunk.co.uk>
2012-10-17 21:48:24 +0100

    Create Changelog

---
+++ changelog.php
@@ -0,0 +1,28 @@
+<?php
+// no direct access
+die('Restricted access');
+
+?>
+
+Changelog
+------------
+
+* -> Security Fix
+# -> Bug Fix
+$ -> Language fix or change
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+1.0.2
+# Fixed bug with the way download count printed in the table
+# Parameter created to show how many weeks data to show
+
+1.0.1 (12/10/2012)
+# Error with the axis Issue fixed
+# For large y-axis is fixed
++ Readme created
+
+1.0.0
++ Initial Release
