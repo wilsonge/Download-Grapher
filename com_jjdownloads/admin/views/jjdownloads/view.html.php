4efd7be4edf7dc0e3bfbee66cfd0202fa3fc92d5
George Wilson <georgejameswilson@googlemail.com>
2014-02-17 17:53:02 +0000

    Updates to JHtml stylesheet inclusion

--- com_jjdownloads/admin/views/jjdownloads/view.html.php
+++ com_jjdownloads/admin/views/jjdownloads/view.html.php
@@ -7,29 +7,26 @@
 
 defined('_JEXEC') or die();
 
-jimport('joomla.application.component.view');
 jimport('joomla.environment.browser');
 
-class jjdownloadsViewjjdownloads extends JView
+class jjdownloadsViewjjdownloads extends JViewLegacy
 {
 	public function display($tpl = null)
 	{
-		$document =& JFactory::getDocument();
-		$document->addScript("http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js");
-		$document->addScript("components/com_jjdownloads/assets/js/visualize.jQuery.js");
-		JHtml::stylesheet('jjdownloads.css', 'administrator/components/com_jjdownloads/assets/css/');
-		JHtml::stylesheet('visualize.css', 'administrator/components/com_jjdownloads/assets/css/');
+		JHtml::_('jquery.framework');
+		JHtml::_('script', JUri::root() . 'administrator/components/com_jjdownloads/assets/js/visualize.jQuery.js');
+		//JHtml::_('stylesheet', JUri:root() . 'administrator/components/com_jjdownloads/assets/css/jjdownloads.css');
+		JHtml::_('stylesheet', JUri::root() . 'administrator/components/com_jjdownloads/assets/css/visualize.css');
 
-		$doc = JFactory::getDocument();
 		$browser = JBrowser::getInstance();
 		$browserType = $browser->getBrowser();
 
 		if ($browserType == 'msie')
 		{
-			$doc->addScript('components/com_jjdownloads/assets/js/excanvas.js');
+			JHtml::_('script', JUri::root() . 'administrator/components/com_jjdownloads/assets/js/excanvas.js');
 		}
-
-		$history = & $this->get('History');
+		
+		$history = $this->get('History');
 		$this->assignRef('history',	$history);
 		JToolBarHelper::title(
 			JText::_('COM_JJ_DOWNLOADS_LAST') . ' ' . $weeks = JComponentHelper::getParams('com_jjdownloads')->get('weeks', 5) .
