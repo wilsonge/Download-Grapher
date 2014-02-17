1121e35d825be6fd39e191b654a95667e2536d9b
George Wilson <georgejameswilson@googlemail.com>
2014-02-17 19:20:02 +0000

    Updates to main entry point and controller

--- com_jjdownloads/admin/controller.php
+++ com_jjdownloads/admin/controller.php
@@ -6,13 +6,18 @@
  **/
 defined('_JEXEC') or die('Direct Access.');
 
+// import Joomla controller library
 jimport('joomla.application.component.controller');
 
-class jjdownloadsController extends JController
+class jjdownloadsController extends JControllerLegacy
 {
 
-	public function display()
+	public function display($cachable = false, $urlparams = false)
 	{
+		// Set default view if not set
+		$input = JFactory::getApplication()->input;
+		$input->set('view', $input->getCmd('view', 'jjdownloads'));
+
 		JSubMenuHelper::addEntry(
 			JText::_('COM_JJ_DOWNLOADS_LAST') . ' ' .
 			$weeks = JComponentHelper::getParams('com_jjdownloads')->get('weeks', 5) . ' ' . JText::_('COM_JJ_DOWNLOADS_WEEKS'),
@@ -20,6 +25,6 @@ class jjdownloadsController extends JController
 		);
 
 		JSubMenuHelper::addEntry(JText::_('COM_JJ_DOWNLOADS_TOTALS'), 'index.php?option=com_jjdownloads&amp;view=totaldownloads');
-		parent::display();
+		parent::display($cachable, $urlparams);
 	}
 }
--- com_jjdownloads/admin/jjdownloads.php
+++ com_jjdownloads/admin/jjdownloads.php
@@ -6,28 +6,19 @@
  **/
 // no direct access
 defined('_JEXEC') or die('Restricted access');
-
-// Require the base controller
-require_once (JPATH_COMPONENT.DS.'controller.php');
-
-// Require specific controller if requested
-if($controller = JRequest::getWord('controller')) {
-    $path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
-    if (file_exists($path)) {
-        require_once $path;
-    } else {
-        $controller = '';
-    }
-}
-
-// Create the controller
-$classname	= 'jjdownloadsController'.$controller;
-$controller = new $classname( );
-
+ 
+// import joomla controller library
+jimport('joomla.application.component.controller');
+ 
+// Get an instance of the controller prefixed by jjdownloads
+$controller = JControllerLegacy::getInstance('jjdownloads');
+ 
+// Get the task
+$jinput = JFactory::getApplication()->input;
+$task = $jinput->get('task', "", 'STR' );
+ 
 // Perform the Request task
-$controller->execute( JRequest::getVar('task'));
-
+$controller->execute($task);
+ 
 // Redirect if set by the controller
 $controller->redirect();
-
-?>
