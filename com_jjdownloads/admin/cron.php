f2d4b4f8463c3f58489753291bdd371ad05a9f5d
George Wilson <georgejameswilson@googlemail.com>
2014-03-17 02:30:23 +0000

    Add doc block for constructor

--- com_jjdownloads/admin/cron.php
+++ com_jjdownloads/admin/cron.php
@@ -69,6 +69,23 @@ class Jjdownloadupdate extends JApplicationCli
 	 */
 	protected $historyTable;
 
+	/**
+	 * Class constructor.
+	 *
+	 * @param   mixed  $input       An optional argument to provide dependency injection for the application's
+	 *                              input object.  If the argument is a JInputCli object that object will become
+	 *                              the application's input object, otherwise a default input object is created.
+	 * @param   mixed  $config      An optional argument to provide dependency injection for the application's
+	 *                              config object.  If the argument is a JRegistry object that object will become
+	 *                              the application's config object, otherwise a default config object is created.
+	 * @param   mixed  $dispatcher  An optional argument to provide dependency injection for the application's
+	 *                              event dispatcher.  If the argument is a JEventDispatcher object that object will become
+	 *                              the application's event dispatcher, if it is null then the default event dispatcher
+	 *                              will be created based on the application's loadDispatcher() method.
+	 *
+	 * @see     JApplicationBase::loadDispatcher()
+	 * @since   11.1
+	 */
 	public function __construct(JInputCli $input = null, JRegistry $config = null, JEventDispatcher $dispatcher = null)
 	{
 		parent::__construct($input, $config, $dispatcher);
