676c58af0d2ad0b0fb437249c28c34cd6782fb17
George Wilson <georgejameswilson@googlemail.com>
2015-04-09 11:36:39 +0100

    Use Joomla way of getting database object

--- com_jjdownloads/admin/models/jjdownloads.php
+++ com_jjdownloads/admin/models/jjdownloads.php
@@ -12,43 +12,6 @@ class jjdownloadsModeljjdownloads extends JModelLegacy
 	var $_data;
 	
 	/**
-	 * The database object
-	 *
-	 * @var    JDatabase
-	 * @since  2.0
-	 */
-	protected $db = null;
-
-	/**
-	 * Constructor
-	 *
-	 * @param   array  $config  A named configuration array for object construction.<br/>
-	 *                          name: the name (optional) of the view (defaults to the view class name suffix).<br/>
-	 *                          charset: the character set to use for display<br/>
-	 *                          escape: the name (optional) of the function to use for escaping strings<br/>
-	 *                          base_path: the parent path (optional) of the views directory (defaults to the component folder)<br/>
-	 *                          template_plath: the path (optional) of the layout directory (defaults to base_path + /views/ + view name<br/>
-	 *                          helper_path: the path (optional) of the helper files (defaults to base_path + /helpers/)<br/>
-	 *                          layout: the layout (optional) to use to display the view<br/>
-	 *                          db: the database object<br/>
-	 *
-	 * @since   2.0
-	 */
-	public function __construct($config = array())
-	{
-		if (array_key_exists('name', $config))
-		{
-			$this->db = $config['db'];
-		}
-		else
-		{
-			$this->db = JFactory::getDbo();
-		}		
-		
-		parent::__construct($config);
-	}
-
-	/**
 	 * Builds the query to the database
 	 *
 	 * @since 1.0
@@ -56,7 +19,7 @@ class jjdownloadsModeljjdownloads extends JModelLegacy
 	 */
 	private function _buildQuery()
 	{
-		$query = $this->db->getQuery(true);
+		$query = $this->getDbo()->getQuery(true);
 		$query->select('*')
 			->from($query->quoteName('#__jjdownloads_history'))
 			->order($query->quoteName('id'));
@@ -84,7 +47,7 @@ class jjdownloadsModeljjdownloads extends JModelLegacy
 	 */
 	public function Name($number)
 	{
-		$query = $this->db->getQuery(true);
+		$query = $this->getDbo()->getQuery(true);
 		$query->select('*')
 			->from($query->quoteName('#__jjdownloads'))
 			->where($query->quoteName('id') . ' = ' . (int) $number);
